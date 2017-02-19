b30c3da242cec686817096e3b32998135fa3c98c
Aditya Kamble <adityakamble49@gmail.com>
2017-02-19 22:03:00 +0530

    Default Timer set to 450 min

--- app/src/main/java/com/adityakamble49/ttl/utils/Constants.java
+++ app/src/main/java/com/adityakamble49/ttl/utils/Constants.java
@@ -3,7 +3,7 @@ package com.adityakamble49.ttl.utils;
 public class Constants {
     public class Timer {
         public static final String KEY_TIMER_MILLIS = "timer_minutes";
-        public static final long TIMER_MILLIS_DEFAULT = 80000;
+        public static final long TIMER_MILLIS_DEFAULT = 450*60*1000;
         public static final String KEY_COUNTDOWN = "count_down";
         public static final int KEY_NOTIFICATION_ID = 1357;
         public static final String KEY_IN_TIME = "in_time";
